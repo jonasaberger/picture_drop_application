609fc75c5ddcc3ba163a08af8acd85e48e7ba326
jonasaberger <145124362+jonasaberger@users.noreply.github.com>
2025-03-21 13:31:19 +0100

    Update API title and description for Picture Drop backend

--- picture-drop-backend/src/main.ts
+++ picture-drop-backend/src/main.ts
@@ -7,8 +7,8 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
-    .setTitle('Todo API')
-    .setDescription('The Todo API description')
+    .setTitle('Picture Drop API')
+    .setDescription('Backend Endpoints for Picture Drop')
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
