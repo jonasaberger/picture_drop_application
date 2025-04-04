fdc75326fb168e5309f1c28f4b485add5e22409e
Jo1234-5 <145123413+Jo1234-5@users.noreply.github.com>
2025-04-04 13:12:17 +0200

    Implemented menu 2

--- picture-drop-frontend/src/app/app.routes.ts
+++ picture-drop-frontend/src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { CouponsComponent} from './coupons/coupons.component';
+import { WorkspacesComponent} from './workspaces/workspaces.component';
 
 export const routes: Routes = [
- //{ path: 'workspaces', component: WorkspacesComponent },
+ { path: 'workspaces', component: WorkspacesComponent },
   { path: 'coupons', component: CouponsComponent },
  // { path: 'statistics', component: StatisticsComponent },
   { path: '', redirectTo: 'workspaces', pathMatch: 'full' }, // Default-Route
