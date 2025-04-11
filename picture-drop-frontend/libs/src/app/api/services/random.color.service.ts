39cf065b017ca834eb86e3a0a4fb5112f793aa10
jonasaberger <145124362+jonasaberger@users.noreply.github.com>
2025-04-11 15:14:20 +0200

    - Finished up statistics

---
+++ picture-drop-frontend/libs/src/app/api/services/random.color.service.ts
@@ -0,0 +1,15 @@
+export function getRandomColor(seed: number): string {
+  const hash = hashCode(seed.toString());
+  const r = (hash & 0xFF0000) >> 16;
+  const g = (hash & 0x00FF00) >> 8;
+  const b = hash & 0x0000FF;
+  return `rgba(${r}, ${g}, ${b}, 0.7)`;
+}
+
+function hashCode(str: string): number {
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  return hash;
+}
